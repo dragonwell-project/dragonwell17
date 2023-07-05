f93648893ca2f95b2246f7f6a076352f40fcc31d
tjw378335 <tjw378335@alibaba-inc.com>
2023-07-05 10:17:10 +0800

    [CDS] Eliminate the impact of CDS loading failure
    Summary:
    When we fail to load the class from sharing archive file, this class
    should continue to be loaded according to the normal process.
    
    In order to avoid affecting the normal process, clear the exception
    during the loading process from CDS.
    
    Test Plan:
    test/jdk/com/alibaba/cds/TestDumpAndLoadClassWithException.java
    
    Reviewed-by: lei.yul, lingjun-cg
    
    Issue:
    CR:

--- src/hotspot/share/classfile/systemDictionaryShared.cpp
+++ src/hotspot/share/classfile/systemDictionaryShared.cpp
@@ -1169,7 +1169,7 @@ bool SystemDictionaryShared::check_class_not_found(const Symbol *class_name, int
                          vmSymbols::isNotFound_name(),
                          vmSymbols::isNotFound_signature(),
                          &args,
-                         CHECK_false);
+                         CHECK_AND_CLEAR_false);
 
   return result.get_jboolean();
 }
@@ -1202,7 +1202,7 @@ InstanceKlass* SystemDictionaryShared::load_class_from_cds(const Symbol* class_n
                           vmSymbols::loadClassFromCDS_name(),
                           vmSymbols::loadClassFromCDS_signature(),
                           &args,
-                          CHECK_NULL);
+                          CHECK_AND_CLEAR_NULL);
 
   oop klass = (oop)result.get_oop();
   if (klass != NULL) {
