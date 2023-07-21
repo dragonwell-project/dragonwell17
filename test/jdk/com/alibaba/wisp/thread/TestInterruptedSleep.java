c28b875c09ac37050844b8b18c8d24938435a67d
Zhai Mo <zhaimo03@kuaishou.com>
2023-07-21 10:29:00 +0800

    [Wisp] Unify the naming rule for Wisp cases in JDK17.
    Summary:
    unify the naming rule for Wisp cases in JDK17
    with the format Test<name>.java
    
    Test Plan: all wisp cases
    
    Reviewed-by: yulei
    
    Issue:
    https://github.com/dragonwell-project/dragonwell17/issues/99

---
+++ test/jdk/com/alibaba/wisp/thread/TestInterruptedSleep.java
@@ -0,0 +1,36 @@
+/*
+ * @test
+ * @summary test InterruptedException was thrown by sleep()
+ * @library /test/lib
+ * @run main/othervm -XX:+EnableCoroutine -Dcom.alibaba.wisp.transparentWispSwitch=true TestInterruptedSleep
+ * @run main/othervm -XX:+EnableCoroutine -Dcom.alibaba.wisp.transparentWispSwitch=true -Dcom.alibaba.wisp.version=2 TestInterruptedSleep
+*/
+
+import com.alibaba.wisp.engine.WispEngine;
+
+import static jdk.test.lib.Asserts.assertFalse;
+import static jdk.test.lib.Asserts.assertLessThan;
+import static jdk.test.lib.Asserts.assertTrue;
+
+public class TestInterruptedSleep {
+    public static void main(String[] args) {
+        Thread mainCoro = Thread.currentThread();
+        WispEngine.dispatch(() -> {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+            }
+            mainCoro.interrupt();
+        });
+        long start = System.currentTimeMillis();
+        boolean ie = false;
+        try {
+            Thread.sleep(1000L);
+        } catch (InterruptedException e) {
+            ie = true;
+        }
+        assertLessThan((int) (System.currentTimeMillis() - start), 1000);
+        assertTrue(ie);
+        assertFalse(mainCoro.isInterrupted());
+    }
+}
