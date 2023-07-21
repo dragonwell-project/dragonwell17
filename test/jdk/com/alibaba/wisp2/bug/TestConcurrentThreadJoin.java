298b24a0b35f6d38ea7f1ec29bce49e84d01c5a0
Zhai Mo <zhaimo03@kuaishou.com>
2023-07-21 10:29:00 +0800

    [Wisp] Unify the naming rule for Wisp cases in JDK17.
    Summary:
    unify the naming rule for Wisp cases in JDK17
    with the format Test<name>.java
    
    Test Plan: all wisp cases
    
    Reviewed-by: yulei
    
    Issue:
    https://github.com/dragonwell-project/dragonwell17/issues/99

---
+++ test/jdk/com/alibaba/wisp2/bug/TestConcurrentThreadJoin.java
@@ -0,0 +1,22 @@
+/*
+ * @test
+ * @library /test/lib
+ * @summary ensure thread.isAlive() is false after thread.join()
+ * @run main/othervm -XX:+UseWisp2 TestConcurrentThreadJoin
+ */
+
+import static jdk.test.lib.Asserts.assertFalse;
+
+public class TestConcurrentThreadJoin {
+    public static void main(String[] args) throws Exception {
+        long start = System.currentTimeMillis();
+
+        while (System.currentTimeMillis() - start < 3000) {
+            Thread thread = new Thread(() -> {
+            });
+            thread.start();
+            thread.join(1000);
+            assertFalse(thread.isAlive());
+        }
+    }
+}
---
+++ test/jdk/com/alibaba/wisp2/bug/TestIsInNative.java
@@ -0,0 +1,46 @@
+/*
+ * @test
+ * @library /test/lib
+ * @summary test Thread.isInNative() is correct
+ * @modules java.base/jdk.internal.access
+ * @run main/othervm -XX:+EnableCoroutine TestIsInNative
+ */
+
+import jdk.internal.access.SharedSecrets;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+
+import static jdk.test.lib.Asserts.assertFalse;
+import static jdk.test.lib.Asserts.assertTrue;
+
+public class TestIsInNative {
+    public static void main(String[] args) throws Exception {
+        Thread nthread = new Thread(() -> {
+            try {
+                SocketChannel ch = SocketChannel.open(new InetSocketAddress("www.example.com", 80));
+                ch.read(ByteBuffer.allocate(4096));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        nthread.start();
+        Thread thread = new Thread(() -> {
+            while (true) {
+
+            }
+        });
+        thread.start();
+        Thread thread2 = new Thread(() -> {
+        });
+        thread2.start();
+        Thread.sleep(500);
+        assertFalse(SharedSecrets.getJavaLangAccess().isInSameNative(nthread));
+        assertFalse(SharedSecrets.getJavaLangAccess().isInSameNative(thread));
+        assertFalse(SharedSecrets.getJavaLangAccess().isInSameNative(thread2));
+        Thread.sleep(1000);
+        assertFalse(SharedSecrets.getJavaLangAccess().isInSameNative(thread));
+    }
+}
