2c13b08cadf302417ad8e32b8cd34ef6718fcbb8
Zhai Mo <zhaimo03@kuaishou.com>
2023-08-25 14:45:18 +0800

    [Wisp] Dump coroutine classes offsets for CDS by default.
    Summary: If classes.jsa is not generated with EnableCoroutine option, it can not be used by Wisp (which will throw an error). By default a classes.jsa will be generated in <JAVA_HOME>/lib/server folder, so we need to dump coroutine classes offsets by default.
    
    Test Plan: all Wisp tests and all CDS tests
    
    Reviewed-by: yulei
    
    Issue:
    https://github.com/dragonwell-project/dragonwell17/issues/140

--- src/hotspot/share/classfile/javaClasses.cpp
+++ src/hotspot/share/classfile/javaClasses.cpp
@@ -4731,7 +4731,7 @@ void java_dyn_CoroutineBase::compute_offsets() {
 }
 
 #if INCLUDE_CDS
-void java_dyn_CoroutineBase::serialize(SerializeClosure* f) {
+void java_dyn_CoroutineBase::serialize_offsets(SerializeClosure* f) {
   COROUTINEBASE_FIELDS_DO(FIELD_SERIALIZE_OFFSET);
 }
 #endif
@@ -4756,7 +4756,7 @@ void com_alibaba_wisp_engine_WispCarrier::compute_offsets() {
 }
 
 #if INCLUDE_CDS
-void com_alibaba_wisp_engine_WispCarrier::serialize(SerializeClosure* f) {
+void com_alibaba_wisp_engine_WispCarrier::serialize_offsets(SerializeClosure* f) {
   WISPENGINE_FIELDS_DO(FIELD_SERIALIZE_OFFSET);
 }
 #endif
@@ -4793,7 +4793,7 @@ void com_alibaba_wisp_engine_WispTask::compute_offsets() {
 }
 
 #if INCLUDE_CDS
-void com_alibaba_wisp_engine_WispTask::serialize(SerializeClosure* f) {
+void com_alibaba_wisp_engine_WispTask::serialize_offsets(SerializeClosure* f) {
   WISPTASK_FIELDS_DO(FIELD_SERIALIZE_OFFSET);
 }
 #endif
@@ -5177,11 +5177,7 @@ void java_lang_InternalError::serialize_offsets(SerializeClosure* f) {
 
 // Compute field offsets of all the classes in this file
 void JavaClasses::compute_offsets() {
-  if (EnableCoroutine) {
-    java_dyn_CoroutineBase::compute_offsets();
-    com_alibaba_wisp_engine_WispCarrier::compute_offsets();
-    com_alibaba_wisp_engine_WispTask::compute_offsets();
-  }
+  BASIC_JAVA_CLASSES_DO_PART_COROUTINE(DO_COMPUTE_OFFSETS);
 
   if (UseSharedSpaces) {
     JVMTI_ONLY(assert(JvmtiExport::is_early_phase() && !(JvmtiExport::should_post_class_file_load_hook() &&
@@ -5205,6 +5201,7 @@ void JavaClasses::compute_offsets() {
 
 void JavaClasses::serialize_offsets(SerializeClosure* soc) {
   BASIC_JAVA_CLASSES_DO(DO_SERIALIZE_OFFSETS);
+  BASIC_JAVA_CLASSES_DO_PART_COROUTINE(DO_SERIALIZE_OFFSETS);
 }
 #endif
 
