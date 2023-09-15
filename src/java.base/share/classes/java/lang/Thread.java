33b8fa8ab6847bd6bcd50666319c115ff94ffa27
Zhai Mo <zhaimo03@kuaishou.com>
2023-09-15 16:43:07 +0800

    [Wisp] Adapt Thread.stackSize.
    Summary:
    Previously wisp uses 512k as the stack size and ignores Thread.stackSize.
    This PR will make wisp to use Thread.stackSize as its stack size.
    
    Test Plan: jtreg:com/jdk/alibaba/wisp2/
    
    Reviewed-by: yulei
    
    Issue:
    https://github.com/dragonwell-project/dragonwell17/issues/153

--- src/java.base/share/classes/java/lang/Thread.java
+++ src/java.base/share/classes/java/lang/Thread.java
@@ -923,7 +923,7 @@ public class Thread implements Runnable {
         boolean started = false;
         try {
             if (!(WEA != null && WispEngine.enableThreadAsWisp() &&
-                    WEA.tryStartThreadAsWisp(this, target))) {
+                    WEA.tryStartThreadAsWisp(this, target, this.stackSize))) {
                 start0();
             }
             started = true;
